680919ae615ee67b9bb68872db1f9e04165f09ca
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-03-19 03:06:27 -0400

    Initial Commit

---
+++ WorkoutBuddy/WorkoutBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutBuddyApp.swift
+//  WorkoutBuddy
+//
+//  Created by Howard Thomas on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
